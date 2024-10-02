d946e321a2e03b8bbfa8a1f79fd30d406165a850
Korede Bashir <bashirkorede@gmail.com>
2024-10-02 16:23:04 +0100

    Update route.ts

--- app/api/submit/route.ts
+++ app/api/submit/route.ts
@@ -1,32 +1,30 @@
+import { google } from 'googleapis';
+
 export async function POST(request: Request) {
   const { email } = await request.json();
 
-  try {
-    const response = await fetch(
-      `https://emailoctopus.com/api/1.6/lists/${process.env.LIST_ID}/contacts`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          api_key: `${process.env.EMAILOCTOPUS_KEY}`,
-          email_address: email,
-        }),
-      }
-    );
+  const auth = new google.auth.GoogleAuth({
+    keyFile: 'app/api/submit/service-account-c96d64a0c721.json',
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+  });
+  
+  const sheets = google.sheets({ version: 'v4', auth });
 
-    if (response.status === 200) {
-      return new Response("Email submitted successfully!", {
-        status: 200,
-      });
-    } else {
-      return new Response("Failed to submit email!", {
-        status: response.status,
-        statusText: response.statusText,
-      });
-    }
+  try {
+    const response = await sheets.spreadsheets.values.append({
+      spreadsheetId: process.env.NEXT_PUBLIC_SPREADSHEET_ID, // Your spreadsheet ID
+      range: 'Sheet1!B2:B', // The range you want to write to
+      valueInputOption: 'USER_ENTERED',
+      requestBody: {
+        values: [[email]],
+      },
+    });
+
+    return new Response('Email submitted to Google Sheets successfully!', {
+      status: 200,
+    });
   } catch (error) {
-    return new Response("Internal server error", { status: 500 });
+    console.error(error);
+    return new Response('Failed to submit email to Google Sheets!', { status: 500 });
   }
 }
