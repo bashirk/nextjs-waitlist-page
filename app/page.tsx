d6eea20b8e5b2b51a7a56972acd80ed4f81ff2a0
Korede <bashirkorede@gmail.com>
2024-10-06 20:41:12 +0100

    replaced promo image

--- app/page.tsx
+++ app/page.tsx
@@ -13,13 +13,13 @@ export default function Home() {
             src="/mobile-app.png"
             alt="Mobile App Screenshots"
             fill
-            className="object-contain mt-8 md:mt-24 px-14 object-bottom"
+            className="object-contain mt-8 md:mt-24 px-14 object-center"
           />
         </div>
 
         <main className="flex flex-col gap-8 mt-8 justify-center px-6 pb-10">
           <h1 className="font-semibold tracking-tight text-zinc-900 text-3xl leading-tight md:text-4xl max-w-lg">
-            Supercharge your user engagement with locally grounded chatbots!
+            Supercharge your user engagement with locally-grounded chatbots!
           </h1>
           <p className="text-gray-500">
             Join the waitlist to be an early adopter!
